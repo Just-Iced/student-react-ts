d5a7ca2282f22d10e98c73148f00d7cfc200c2eb
Just-Iced <plunkettjustice@gmail.com>
2026-02-12 14:02:49 -0800

    Fixed TS issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import "bootstrap/dist/css/bootstrap.css";
 import { Outlet } from "react-router"
-import NavBar from './components/NavBar';
+import NavBar from "./components/NavBar";
 
 function App() {
+
   return (
     <>
       <div className="container">
         <NavBar />
-        <Outlet title={"Hello, welcome to students-react with Typescript!"}
-          subtitle={"You can view some of your favorite students here."}>
-          Surely, school is fun.
-        </Outlet>
-
-      </div></>
+        <Outlet />
+      </div>
+    </>
   );
 }
+
 export default App;
